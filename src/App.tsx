90d4e6648b81accbea0a5ae3cd27888fb572b245
Arsalan Ghogari <arsalan.m.ghogari@gmail.com>
2025-01-27 20:46:13 -0800

    Final v2

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,24 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import EBoard from "./pages/EBoard";
 import Join from "./pages/Join";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+// ScrollToTop component
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top on route change
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
