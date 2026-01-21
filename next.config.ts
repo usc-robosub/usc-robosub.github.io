886ed2f76f3d665a398817f90937d4dec2f1ed7b
Alejandro Arteaga <ajarteag@usc.edu>
2026-01-21 12:57:57 -0800

    added deploy.yml for github actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Recommended for GitHub Pages
+  },
 };
 
 export default nextConfig;
